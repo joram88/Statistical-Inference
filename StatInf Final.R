28fd7cdcc593ab948cf2ba16724a2b86d12545f3
jrpineda <53352598+jrpineda@users.noreply.github.com>
2019-12-09 11:41:33 -0500

    Update StatInf Final.R

--- StatInf Final.R
+++ StatInf Final.R
@@ -26,11 +26,13 @@ testvar
 #variance we got in the test (5.99)
 
 ggplot(data = meantest, aes(x = means)) + 
-  geom_histogram(binwidth=0.1, aes(y=..density..), alpha=0.2) + 
-  geom_vline(xintercept = mu, size=1, colour="red") + 
-  geom_density(colour="blue", size=1) +
-  scale_x_continuous(breaks=seq(mu-5,mu+5,1), limits=c(mu-5,mu+6))  +
-  stat_function(fun = dnorm, args = list(mean = mu , sd = sqrt(var)), colour = "red", size=1) 
+        geom_histogram(binwidth=0.1, aes(y=..density..), alpha=0.2) + 
+        geom_vline(xintercept = mu, size=1, colour="red") + 
+        geom_density(colour="blue", size=1) +
+        scale_x_continuous(breaks=seq(mu-5,mu+5,1), limits=c(mu-5,mu+6))  +
+        labs(title = "Is this a normal distribution?", subtitle = "At first glance, yes") +
+        stat_function(fun = dnorm, args = list(mean = mu , sd = sqrt(var)), colour = "red", size=1)
+        
 
 #Graphically this seems like a normal distribution, but lets run a Shapiro test to confirm
 
@@ -42,7 +44,7 @@ shapiro.test(meantest$means)
 #fall outside the confidence interval testing normality, which explains the Shapiro test results
 
 
-ggqqplot(meantest$means)
+ggqqplot(meantest$means, title = "QQ plot")
 
 #######################################################################
 
@@ -55,12 +57,12 @@ tooth <- ToothGrowth
 # Vitamins were administered via orange juice (OJ) or ascorbic acid (VC)
 
 g <- ggplot(tooth, aes(x = supp, y = len, fill = supp))
-g +geom_boxplot()
+g +geom_boxplot()+labs(title = "Supplement effect on tooth length")
 
 tooth$dose <- as.factor(tooth$dose)
 
 h <- ggplot(tooth, aes(x = dose, y = len, fill = dose))
-h +geom_boxplot()
+h +geom_boxplot()+labs(title = "Dose effect on tooth length")
 
 #We can tell from our early graphs that VC seems to have a weaker effect on len than
 #if OC was administered. Also, unsurprisingly, if the dose is higher the effect is larger
@@ -81,7 +83,9 @@ mean(permutations > observedStat)
 # The p - value is small though still, so we reject the null hypothesis at an alpha of 0.05
 
 ggplot()+aes(permutations)+geom_histogram(binwidth=1, color="lightblue")+
-    geom_vline(aes(xintercept=3.7, color="red", size =0.05), show.legend = FALSE)
+        geom_vline(aes(xintercept=3.7, color="red", size =0.05), show.legend = FALSE)+
+        labs(title = "Permutation Distribution")
+        
 
 # From our histogram we see that most observations fall below that mean we calculated of 3.7.
 # So a 0 mean difference is very unlikely.
@@ -102,22 +106,22 @@ t.test(difference, paired = FALSE)
 #Now we do the same for dose for both supplements separately
 
 h1oj <- tooth %>% 
-      filter(supp == "OJ") %>% 
-      arrange(dose) %>% 
-      slice(1:10) %>% 
-      select(len)
+        filter(supp == "OJ") %>% 
+        arrange(dose) %>% 
+        slice(1:10) %>% 
+        select(len)
 
 h2oj <- tooth %>% 
-  filter(supp == "OJ") %>% 
-  arrange(dose) %>% 
-  slice(11:20) %>%
-  select(len)
+        filter(supp == "OJ") %>% 
+        arrange(dose) %>% 
+        slice(11:20) %>%
+        select(len)
 
 h3oj <- tooth %>%
-  filter(supp == "OJ") %>% 
-  arrange(dose) %>% 
-  slice(21:30) %>% 
-  select(len)
+        filter(supp == "OJ") %>% 
+        arrange(dose) %>% 
+        slice(21:30) %>% 
+        select(len)
 
 difference2oj <- h3oj-h2oj
 
@@ -133,22 +137,22 @@ t.test(difference3oj, paired = FALSE)
 #positive, with a very low p-value and high CI.
 
 h1vc <- tooth %>% 
-  filter(supp == "VC") %>% 
-  arrange(dose) %>% 
-  slice(1:10) %>% 
-  select(len)
+        filter(supp == "VC") %>% 
+        arrange(dose) %>% 
+        slice(1:10) %>% 
+        select(len)
 
 h2vc <- tooth %>% 
-  filter(supp == "VC") %>% 
-  arrange(dose) %>% 
-  slice(11:20) %>%
-  select(len)
+        filter(supp == "VC") %>% 
+        arrange(dose) %>% 
+        slice(11:20) %>%
+        select(len)
 
 h3vc <- tooth %>% 
-  filter(supp == "VC") %>% 
-  arrange(dose) %>% 
-  slice(21:30) %>% 
-  select(len)
+        filter(supp == "VC") %>% 
+        arrange(dose) %>% 
+        slice(21:30) %>% 
+        select(len)
 
 difference2vc <- h3vc-h2vc
 
@@ -160,7 +164,5 @@ t.test(difference3vc, paired = FALSE)
 
 #As for VC we can see that increased dosing has a very statstically significant positive effect
 #on length, with high/positive CI and p-values that are extremely low. The Ho is rejected.
-
-
 
 
